783e69ee9873fa7c413412e48d1d552982518967
Erick <erick.cr04@hotmail.com>
2023-05-13 22:28:57 -0600

    Practica 11
    uso de google maps, key en environment, componente de google en modal en destinos, obtener direction desde capacitor, extender window

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -1,6 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
@@ -11,14 +8,6 @@ export const environment = {
     storageBucket: "pruebas-labapimov.appspot.com",
     messagingSenderId: "249172033778",
     appId: "1:249172033778:web:06b2f4ed01d281cc1bd06b",
-  }
+  },
+  apiKeyMaps : "AIzaSyCsaOkBr3wqaRyQwPJJ4kFcy--DH8bz4Bo"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
